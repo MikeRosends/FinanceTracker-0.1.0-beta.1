6defc4f388eec1756406370df6ac6201ecdb2f1e
Miguel Rosendo <miguelrosendo29@gmail.com>
2024-06-04 09:20:30 +0100

    Added lists: Wishlish and Deposits

--- FirstProject/src/main/java/com/MikeRosends/springapi/model/User.java
+++ FirstProject/src/main/java/com/MikeRosends/springapi/model/User.java
@@ -7,12 +7,19 @@ import java.util.Map;
 public class User {
 
     private int id;
+    private String name;
     private List<Expense> expenses;
+    private List<Wishlist> wishlist;
+    private List<Deposit> deposits;
 
 
-    public User(int id, List<Expense> expenses) {
+    public User(int id, String name, List<Deposit> deposits, List<Expense> expenses, List<Wishlist> wishlist) {
         this.id = id;
+        this.name = name;
+        this.deposits = deposits;
         this.expenses = expenses;
+        this.wishlist = wishlist;
+        this.deposits = deposits;
     }
 
     public int getId() {
@@ -23,11 +30,35 @@ public class User {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Deposit> getDeposits() {
+        return deposits;
+    }
+
+    public void setDeposits(List<Deposit> deposits) {
+        this.deposits = deposits;
+    }
+
     public List<Expense> getExpenses() {
         return expenses;
     }
 
     public void setExpenses(List<Expense> expenses) {
         this.expenses = expenses;
+    }
+
+    public List<Wishlist> getWishlist() {
+        return wishlist;
+    }
+
+    public void setWishlist(List<Wishlist> wishlist) {
+        this.wishlist = wishlist;
     }
 }
--- FirstProject/src/main/java/com/MikeRosends/springapi/service/UserService.java
+++ FirstProject/src/main/java/com/MikeRosends/springapi/service/UserService.java
@@ -1,7 +1,9 @@
 package com.MikeRosends.springapi.service;
 
+import com.MikeRosends.springapi.model.Deposit;
 import com.MikeRosends.springapi.model.Expense;
 import com.MikeRosends.springapi.model.User;
+import com.MikeRosends.springapi.model.Wishlist;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -11,20 +13,34 @@ public class UserService {
 
     private List<User> userList;
     private List<Expense> expensesList;
+    private List<Wishlist> wishList;
+    private List<Deposit> deposits;
 
     public UserService() {
         userList = new ArrayList<>();
         expensesList = new ArrayList<>();
+        wishList = new ArrayList<>();
+        deposits = new ArrayList<>();
 
         Expense expense1 = new Expense(1, "BJJ", 43);
         Expense expense2 = new Expense(2, "Food", 50);
         Expense expense3 = new Expense(3, "Code4All Contract", 143);
+        Expense expense4 = new Expense(3, "Save - Invest (20% of salary)", 180);
+
+        Wishlist item1 = new Wishlist(1, "Raising Desk", 200);
+        Wishlist item2 = new Wishlist(2, "New Phone (Specific Model)", 599);
+        Wishlist item3 = new Wishlist(3, "Watter Bottle - tupperware", 40);
+
+        Deposit deposit1 = new Deposit(1, "Salary", 900);
+        Deposit deposit2 = new Deposit(2, "MB-Way", 37);
 
 
-        expensesList.addAll(Arrays.asList(expense1, expense2, expense3));
+        expensesList.addAll(Arrays.asList(expense1, expense2, expense3, expense4));
+        wishList.addAll(Arrays.asList(item1, item2, item3));
+        deposits.addAll(Arrays.asList(deposit1, deposit2));
 
-        User user1 = new User(1, expensesList);
-        User user2 = new User(2, expensesList);
+        User user1 = new User(1, "mike", deposits, expensesList, wishList);
+        User user2 = new User(2, "pete", deposits, expensesList, wishList);
 
         userList.addAll(Arrays.asList(user1, user2));
     }
